0bec97e938a0fc6ea5baf75f7872f0b6e0ae575d
bekzat <apasniynegr228@gmail.com>
2023-12-09 12:16:14 +0600

    firebase auth and log

--- app/src/main/java/com/example/kursach3/MainActivity.java
+++ app/src/main/java/com/example/kursach3/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.kursach3;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_email, et_password;
+    Button btn_registration, btn_login_activity;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_email = findViewById(R.id.et_login_email);
+        et_password = findViewById(R.id.et_login_password);
+        btn_registration = findViewById(R.id.btn_login);
+        mAuth = FirebaseAuth.getInstance();
+        btn_login_activity = findViewById(R.id.btn_login_activity);
+
+
+        btn_registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = et_email.getText().toString();
+                password = et_password.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                } else {
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        btn_login_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
