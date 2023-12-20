cd90a067a46af9288bf69001e863f170f7581ea1
osolenye <apasniynegr228@gmail.com>
2023-12-20 08:39:31 +0600

    starting making products and types of products

--- app/src/main/java/com/example/kursach3/waiter/WaiterActivity.java
+++ app/src/main/java/com/example/kursach3/waiter/WaiterActivity.java
@@ -3,10 +3,12 @@ package com.example.kursach3.waiter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.kursach3.R;
 
 public class WaiterActivity extends AppCompatActivity {
+    ListView lv_table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
