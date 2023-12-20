076d494eb3afed5c835bed59dfb10fa29ab95dd7
osolenye <apasniynegr228@gmail.com>
2023-12-20 08:39:40 +0600

    starting making products and types of products

---
+++ app/src/main/java/com/example/kursach3/waiter/LvTablesActivity.java
@@ -0,0 +1,16 @@
+package com.example.kursach3.waiter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kursach3.R;
+
+public class LvTablesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lv_tables2);
+    }
+}
